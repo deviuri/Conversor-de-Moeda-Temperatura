7155219a1526f64b6352b3df9ec8fc0096768e27
iurim134 <114511281+iurim134@users.noreply.github.com>
2023-02-24 23:50:33 -0300

    Update Moedas.java

--- ConvesorDeMoeda/src/Moedas.java
+++ ConvesorDeMoeda/src/Moedas.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class Moedas {
+    DecimalFormat df = new DecimalFormat();
+
     public Moedas(){
+        df.applyPattern("#,##0.00");
 
     Double valor = null;
     Double resultado = null;
@@ -9,61 +13,62 @@ public class Moedas {
         String[] moeda = {"Dólar para Real", "Euro para Real", "Libra Esterlina para Real",
                 "Peso Argentino para Real", "Peso Chileno para Real", "Real para Dólar", "Real para Euro",
                 "Real para Libra Esterlina", "Real para Peso Argentino", "Real para Peso Chileno" };
-        String input = JOptionPane.showInputDialog("Digite seu Valor:");
-                valor = Double.parseDouble(input);
+
 
         String moedas = JOptionPane.showInputDialog(null, "Escolha à moeda para converter:", "Conversor de Moedas",
                 JOptionPane.PLAIN_MESSAGE, null, moeda, null).toString();
+        String input = JOptionPane.showInputDialog("Digite seu Valor:");
+        valor = Double.parseDouble(input);
         Moeda converterMoeda = new Moeda();
                     switch (moedas) {
             case "Dólar para Real": {
                 resultado = converterMoeda.converterDolarEmReal(valor);
-                JOptionPane.showMessageDialog(null, resultado+"R$");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"R$");
             }break;
 
             case "Euro para Real": {
                 resultado = converterMoeda.converterEuroEmReal(valor);
-                JOptionPane.showMessageDialog(null, resultado+"R$");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"R$");
             }break;
 
             case "Libra Esterlina para Real": {
                 resultado = converterMoeda.converterLibraEsterlinaEmReal(valor);
-                JOptionPane.showMessageDialog(null, resultado+"R$");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"R$");
             }break;
 
             case "Peso Argentino para Real": {
                 resultado = converterMoeda.converterPesoArgentinoEmReal(valor);
-                JOptionPane.showMessageDialog(null, resultado+"R$");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"R$");
             }break;
 
             case "Peso Chileno para Real": {
                 resultado = converterMoeda.converterPesoChilenoEmReal(valor);
-                JOptionPane.showMessageDialog(null, resultado+"R$");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"R$");
             }break;
 
             case "Real para Dólar": {
                 resultado = converterMoeda.converterRealEmDolar(valor);
-                JOptionPane.showMessageDialog(null, resultado+"$");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"$");
             }break;
 
             case "Real para Euro": {
                 resultado = converterMoeda.converterRealEmEuro(valor);
-                JOptionPane.showMessageDialog(null, resultado+"€");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"€");
             } break;
 
             case "Real para Libra Esterlina":{
                 resultado = converterMoeda.converterRealEmLibraEsterlina(valor);
-                JOptionPane.showMessageDialog(null, resultado+"£");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"£");
                 }break;
 
             case "Real para Peso Argentino": {
                 resultado = converterMoeda.converterRealEmPesoArgentino(valor);
-                JOptionPane.showMessageDialog(null, resultado+"(ARS)");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"(ARS)");
             }break;
 
             case "Real para Peso Chileno": {
                 resultado = converterMoeda.converterRealEmPesoChileno(valor);
-                JOptionPane.showMessageDialog(null, resultado+"()CLP");
+                JOptionPane.showMessageDialog(null, df.format(resultado)+"(CLP)");
             }break;
 
             default:
